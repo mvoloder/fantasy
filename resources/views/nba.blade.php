d6516175361e64e9b637302fce064a2e3661dbac
vagrant <vagrant@lamp>
2016-11-03 12:36:33 +0000

    create/join layout

---
+++ resources/views/nba.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container">
+
+        <h2>Create or join existing league</h2>
+
+        <div class="btn-group btn-group-justified">
+            <a href="#" class="btn btn-primary">Create league</a>
+            <a href="#" class="btn btn-primary">Join league</a>
+        </div>
+
+    </div>
+
+    <div class="container">
+
+        <h3>List of your leagues</h3>
+
+
+    </div>
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('choosesport');
+    return view('nba');
 });
 
 Auth::routes();
