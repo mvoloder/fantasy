5cea7408508f6bd6399733b546e3a0646190c5ac
mvoloder <marko.voloder@gmail.com>
2016-12-01 14:28:55 +0100

    matchups

--- resources/views/matchup.blade.php
+++ resources/views/matchup.blade.php
@@ -7,12 +7,12 @@
         <tr>
             {{--LEFT SIDE--}}
             <td align="center">
-                <form class="form-horizontal" role="form" method="get">
-                @foreach($tm_settings as $tm_setting)
-                    
-                    {{$tm_setting}} <br>
-                    @endforeach
-                </form>
+                {{--<form class="form-horizontal" role="form" method="get">--}}
+                {{--@foreach($tm_settings as $tm_setting)--}}
+                    {{----}}
+                    {{--{{$tm_setting}} <br>--}}
+                    {{--@endforeach--}}
+                {{--</form>--}}
             </td>
             <td class="divider">
 
@@ -20,11 +20,11 @@
 
             {{--RIGHT SIDE--}}
             <td align="center">
-                <form class="form-horizontal" role="form" method="get">
-                @foreach($tm_settings as $tm_setting)
-                    {{$tm_setting}} <br>
-                @endforeach
-                </form>
+                {{--<form class="form-horizontal" role="form" method="get">--}}
+                {{--@foreach($tm_settings as $tm_setting)--}}
+                    {{--{{$tm_setting}} <br>--}}
+                {{--@endforeach--}}
+                {{--</form>--}}
             </td>
         </tr>
     </table>
